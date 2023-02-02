1d512702d8b35d9ab853eb30a53725c8379b20a6
chandradutt <patelchandraadutt@gmail.com>
2023-02-02 11:55:27 +0530

    annotating objects

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from store.models import Product
+from store.models import Product, Customer, Order
 
 def say_hello(request):
-    queryset = Product.objects.select_related('collection').all()
+    queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
     
-    return render(request, 'hello.html', {'name': 'Mosh', 'products': queryset})
+    return render(request, 'hello.html', {'name': 'Mosh', 'orders': list(queryset)})
